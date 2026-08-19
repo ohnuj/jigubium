c3d118edc0a2d35b4efe8a8c7bd910bb1cfd540e
parkdjhsh-alt <parkdjhsh1@gmail.com>
2026-08-19 17:57:53 +0900

    지도 api 불러오기, 의류수거함 api 불러오기, 폐건전지 api 불러오기 의류수거함과 지도 연동 완료(마커 띄움)
    *인텔리j 환경변수 따로 설정해줘야 함

--- recycle/src/main/java/first_project/recycle/controller/MapControllter.java
+++ recycle/src/main/java/first_project/recycle/controller/MapControllter.java
@@ -1,4 +1,56 @@
 package first_project.recycle.controller;
 
+import first_project.recycle.domain.ecoLocation;
+import first_project.recycle.domain.ecoLocationDTO.JongnoBatteryBinResponse;
+import first_project.recycle.domain.ecoLocationDTO.JongnoClothingBinResponse;
+import first_project.recycle.service.JongnoBatteryBinService;
+import first_project.recycle.service.JongnoClothingBinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
 public class MapControllter {
+
+    //의류수거함 서비스 객체
+    private final JongnoClothingBinService jongnoClothingBinService;
+
+    //폐건전지 수거함 서비스 객체
+    private final JongnoBatteryBinService jongnoBatteryBinService;
+
+    //생성자 주입
+    public MapControllter(
+            JongnoClothingBinService jongnoClothingBinService,
+            JongnoBatteryBinService jongnoBatteryBinService
+    ) {
+        this.jongnoClothingBinService = jongnoClothingBinService;
+        this.jongnoBatteryBinService = jongnoBatteryBinService;
+    }
+
+    //지도 페이지 이동
+    @GetMapping("/eco-map")
+    public String map() {
+        return "forward:/eco-map/recyclemap.html";
+    }
+
+    // api 출력을 도메인 구조에 맞게 변환
+    // 종로구 의류수거함
+    @GetMapping("/api/clothing-bins")
+    @ResponseBody
+    public List<ecoLocation> getClothingBins() {
+        return jongnoClothingBinService.getClothingBins();
+    }
+    // 종로구 폐건전지 수거함 데이터
+    // 현재는 API 응답 DTO를 그대로 반환해서 테스트
+    @GetMapping("/api/battery-bins")
+    @ResponseBody
+    public JongnoBatteryBinResponse getBatteryBins() {
+
+        return jongnoBatteryBinService.getBatteryBins();
+    }
+
+
+
 }
---
+++ recycle/src/main/java/first_project/recycle/domain/ecoLocationDTO/JongnoClothingBinResponse.java
@@ -0,0 +1,18 @@
+package first_project.recycle.domain.ecoLocationDTO;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class JongnoClothingBinResponse {
+    private int page;
+    private  int perPage;
+    private  int totalCount;
+    private  int currentCount;
+
+    private List<JongnoClothingBinDto> data;
+}
